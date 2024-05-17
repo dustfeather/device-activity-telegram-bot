7e56447a58a2a0746f667415d8a71e9907fa9f06
Catalin Teodorescu <dustfeather@gmail.com>
2024-05-17 06:54:34 +0300

    more compile tests

--- send.py
+++ send.py
@@ -2,9 +2,18 @@ import requests
 from dotenv import load_dotenv
 import os
 import platform
+import sys
 
-# Load environment variables from .env file
-load_dotenv()
+def load_env():
+    if getattr(sys, 'frozen', False):
+        # Running in a bundle
+        bundle_dir = sys._MEIPASS
+        env_path = os.path.join(bundle_dir, '.env')
+    else:
+        # Running in normal Python environment
+        env_path = os.path.join(os.path.dirname(__file__), '.env')
+
+    load_dotenv(env_path)
 
 def send(message):
     token = os.getenv('BOT_TOKEN')  # Fetch the bot token from the .env file
@@ -18,6 +27,7 @@ def send(message):
     return response.json()
 
 if __name__ == "__main__":
+    load_env()
     device_name = platform.node()  # Get the device name
     message = f"Your PC '{device_name}' has been logged into."
     send(message)
